2d5ea12857022aa12f3e23d36ca9761f9bbac820
Fullofb3ans <igorgazinskii6@gmail.com>
2024-05-23 01:01:58 +0400

    1com

---
+++ src/components/Vcard.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+const props = defineProps(["product"]);
+
+function checkLength(item){
+    let text = '';
+        for (let i = 0; i < item.length; i++) {
+            if(i <= 50){
+            text += item[i];
+        }
+        }
+    return text;
+}
+</script>
+
+<template>
+<div class="card"  style="width: 17rem;">
+  <img :src="product.image" class="card-img-top" alt="..." style="width: 200px; height: 200px; align-self: center; padding: 2%">
+  <h5 class="card-title" style="    text-align: center;">{{ product.title }}</h5>
+  <div class="card-body">
+    <p class="card-text">{{checkLength(product.description)  + '...'}}</p>
+  </div>
+</div>
+</template>
---
+++ src/components/Vheader.vue
@@ -0,0 +1,26 @@
+<script setup>
+const emits = defineEmits([]);
+
+function loadProducts (){
+  emits('loadProducts');
+}
+function deleteProducts (){
+  emits('deleteProducts');
+}
+
+</script>
+<template>
+  <header>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+   <h3> <button class="nav-link active" @click="deleteProducts" aria-current="page" href="#">Запустить лоадер</button>
+  </h3></div>
+</nav>
+</header>
+</template>
+
+<style scoped>
+.active:hover{
+  color: rgb(125, 17, 142);
+  transition: 0.2s;
+}</style>
